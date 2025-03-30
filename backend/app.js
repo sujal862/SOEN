940d6bc30de28a418431d8c64503625f7f92745e
Sujal Raj <rajsujal2004@gmail.com>
2025-03-31 01:33:07 +0530

    error fix for deploy

--- backend/app.js
+++ backend/app.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 
 app.use(cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    origin: "*",
     credentials: true
 }));
  
